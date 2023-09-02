f1daa0f7e8aec4b34b365df1bfe18084319809b5
unknown <atayarabbi44544@gmail.com>
2023-09-02 16:16:51 +0600

    cart added

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -3,14 +3,25 @@ import './Cart.css';
 
 const Cart = ({ cart }) => {
 
+    let totalPrice = 0;
+    let totalShipping = 0;
+    for (const product of cart) {
+        totalPrice = totalPrice + product.price;
+        totalShipping = totalShipping + product.shipping;
+    }
+
+    const tax = totalPrice * 7 / 100;
+
+    const grandTotal = totalPrice + totalShipping + tax;
+
     return (
-        <div>
+        <div className="cart">
             <h2>Order summary</h2>
-            <p>Selected Items: {cart.length}</p>
-            <p>Total Product Price : { }</p>
-            <p>Shipping Cost : { }</p>
-            <p>Tax Applied: { }</p>
-            <p>Grand Total: { }</p>
+            <p>Selected Items: <span>{cart.length}</span></p>
+            <p>Total Product Price : $<span>{totalPrice}</span></p>
+            <p>Shipping Cost : $<span>{totalShipping}</span></p>
+            <p>Tax Applied: $<span>{tax}</span></p>
+            <h4>Grand Total: <span>{grandTotal}</span></h4>
 
         </div>
     );
--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
-import Cart from '../Cart/Cart';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
@@ -17,7 +17,6 @@ const Shop = () => {
         const newCart = [...cart, product];
         setCart(newCart)
     }
-
 
     return (
         <div className='shop-container'>
